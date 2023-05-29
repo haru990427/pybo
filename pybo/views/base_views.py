71d059697eb85e2d37a5b27abffd800830eddacb
uzjaee <kyj960802@gmail.com>
2023-05-29 18:30:07 +0900

    feat : 2번요구사항: 댓글 페이지 기능 및 정렬기능 구현완료

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
 
 from pybo.models import Question
 
@@ -25,9 +26,15 @@ def index(request):
 
 def detail(request, question_id):
     page = request.GET.get('comment_page', '1')
+    comment_sort = request.GET.get('comment_sort', None)
     question = get_object_or_404(Question, pk=question_id)
-    comment_question_list = question.comment_set.all().order_by('-create_date')
+    comment_question_list = question.comment_set.all()
+    # 정렬
+    if comment_sort == 'recent':  # 최신순
+        comment_question_list = comment_question_list.order_by('-create_date')
+    if comment_sort == 'vote':  # 추천순 , 최신순)
+        comment_question_list = comment_question_list.annotate(vote_count=Count('voter')).order_by('-vote_count','-create_date')
     paginator = Paginator(comment_question_list, 5)
     page_obj = paginator.get_page(page)
-    context = {'question': question , 'comment_list': page_obj}
+    context = {'question': question , 'comment_list': page_obj, 'comment_sort': comment_sort}
     return render(request, 'pybo/question_detail.html', context)
